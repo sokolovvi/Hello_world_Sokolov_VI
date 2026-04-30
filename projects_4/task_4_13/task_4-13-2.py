c1218f4045f21e7bfaf086406c2caab69c17eb98
sokolovvi <sokolov.vi@edu.spbstu.ru>
2026-04-30 04:42:01 +0300

    Delete projects_4 directory

--- projects_4/task_4_13/task_4-13-2.py
+++
@@ -1,7 +0,0 @@
-A = float(input("Введите A: "))
-B = float(input("Введите B: "))
-C = float(input("Введите C: "))
-D = float(input("Введите D: "))
-S = A + B + C + D
-avg = S / 4
-print("Среднее =", avg)
