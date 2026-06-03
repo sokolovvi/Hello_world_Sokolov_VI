0f8886c3fcfd584ad7fe3148a7e87599a0749326
sokolovvi <sokolov.vi@edu.spbstu.ru>
2026-06-04 01:38:44 +0300

    Delete lab_3-3 directory

--- lab_3-3/task_3-12/task_3-12-1-find_user.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-grep "^$USER:" /etc/passwd
--- lab_3-3/task_3-13/task_3-13-1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-sed -i 's|/var/lib/mysql/data|/mnt/ssd/mysql|g' settings.php
-
-echo "✅ Замена выполнена: /var/lib/mysql/data → /mnt/ssd/mysql"
